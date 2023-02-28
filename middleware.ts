b2f983139b7e8a75082d5669521096b760038db3
Domen <domen.lisjak@gmail.com>
2023-02-28 20:42:45 +0100

    add clg

--- middleware.ts
+++ middleware.ts
@@ -17,20 +17,19 @@ export const config = {
 export default async function middleware(req: NextRequest) {
 	const url = req.nextUrl;
 
-	// Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+	// Get hostname of request (e.g. demo.founder.si, demo.localhost:3000)
 	const hostname = req.headers.get('host') || 'demo.founder.si';
-	console.log({ hostname });
 
 	// Get the pathname of the request (e.g. /, /about, /blog/first-post)
 	const path = url.pathname;
 
-	/*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+	/*  You have to replace ".founder.si" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
       in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
       still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
 	const currentHost =
 		process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
-			? hostname.replace(`.founder.si`, '')
+			? hostname.replace(`.founder.si`, '').replace(`.founder.si`, '')
 			: hostname.replace(`.localhost:3000`, '');
 
 	// rewrites for app pages
