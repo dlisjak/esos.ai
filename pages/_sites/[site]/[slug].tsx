d3ca45143fbc0eece13d5d83cff30e721e263e7c
Domen <domen.lisjak@gmail.com>
2023-02-28 18:12:29 +0100

    fix error

--- pages/_sites/[site]/[slug].tsx
+++ pages/_sites/[site]/[slug].tsx
@@ -76,11 +76,7 @@ export default function Post({
 				</div>
 				<a
 					// if you are using Github OAuth, you can get rid of the Twitter option
-					href={
-						data.site?.user?.username
-							? `https://twitter.com/${data.site.user.username}`
-							: `https://github.com/${data.site?.user?.gh_username}`
-					}
+					href={`https://github.com/${data.site?.user?.gh_username}`}
 					rel="noreferrer"
 					target="_blank"
 				>
