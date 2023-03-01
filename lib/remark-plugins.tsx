94a4224b2280e071ca3266b972aa48c0a39d8d71
Domen <domen.lisjak@gmail.com>
2023-03-01 17:10:07 +0100

    add mdx fix

--- lib/remark-plugins.tsx
+++ lib/remark-plugins.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { visit } from 'unist-util-visit';
 
-import { getTweets } from '@/lib/twitter';
+// import { getTweets } from '@/lib/twitter';
 
 import type { Literal, Node } from 'unist';
 import type { Example, PrismaClient } from '@prisma/client';
@@ -27,70 +27,70 @@ export function replaceLinks(options: { href?: string } & WithChildren) {
 	);
 }
 
-export function replaceTweets<T extends Node>() {
-	return (tree: T) =>
-		new Promise<void>(async (resolve, reject) => {
-			const nodesToChange = new Array<NodesToChange>();
+// export function replaceTweets<T extends Node>() {
+// 	return (tree: T) =>
+// 		new Promise<void>(async (resolve, reject) => {
+// 			const nodesToChange = new Array<NodesToChange>();
 
-			visit(tree, 'text', (node: any) => {
-				if (
-					node.value.match(
-						/https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)([^\?])(\?.*)?/g
-					)
-				) {
-					nodesToChange.push({
-						node,
-					});
-				}
-			});
-			for (const { node } of nodesToChange) {
-				try {
-					const data = await getTweet(node as Literal<string>);
+// 			visit(tree, 'text', (node: any) => {
+// 				if (
+// 					node.value.match(
+// 						/https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)([^\?])(\?.*)?/g
+// 					)
+// 				) {
+// 					nodesToChange.push({
+// 						node,
+// 					});
+// 				}
+// 			});
+// 			for (const { node } of nodesToChange) {
+// 				try {
+// 					const data = await getTweet(node as Literal<string>);
 
-					node.type = 'mdxJsxFlowElement';
-					// @ts-expect-error - Node is a generic type, but we are creating a JSX node here
-					node.name = 'Tweet';
-					// @ts-expect-error - Node is a generic type, but we are creating a JSX node here
-					node.attributes = [
-						{
-							type: 'mdxJsxAttribute',
-							name: 'id',
-							value: data.id,
-						},
-						{
-							type: 'mdxJsxAttribute',
-							name: 'metadata',
-							value: data.metadata,
-						},
-					];
-				} catch (e) {
-					console.log('ERROR', e);
-					return reject(e);
-				}
-			}
+// 					node.type = 'mdxJsxFlowElement';
+// 					// @ts-expect-error - Node is a generic type, but we are creating a JSX node here
+// 					node.name = 'Tweet';
+// 					// @ts-expect-error - Node is a generic type, but we are creating a JSX node here
+// 					node.attributes = [
+// 						{
+// 							type: 'mdxJsxAttribute',
+// 							name: 'id',
+// 							value: data.id,
+// 						},
+// 						{
+// 							type: 'mdxJsxAttribute',
+// 							name: 'metadata',
+// 							value: data.metadata,
+// 						},
+// 					];
+// 				} catch (e) {
+// 					console.log('ERROR', e);
+// 					return reject(e);
+// 				}
+// 			}
 
-			resolve();
-		});
-}
+// 			resolve();
+// 		});
+// }
 
-async function getTweet(node: Literal<string>) {
-	const regex = /\/status\/(\d+)/gm;
+// async function getTweet(node: Literal<string>) {
+// 	const regex = /\/status\/(\d+)/gm;
 
-	const matches = regex.exec(node.value);
-	if (!matches) throw new Error(`Failed to get tweet: ${node}`);
+// 	const matches = regex.exec(node.value);
+// 	if (!matches) throw new Error(`Failed to get tweet: ${node}`);
 
-	const id = matches[1];
-	let tweetData = null;
-	try {
-		tweetData = await getTweets(id);
-	} catch (e) {
-		console.log('ERROR', e);
-	}
-	return {
-		id,
-		metadata: JSON.stringify(tweetData),
-	};
-}
+// 	const id = matches[1];
+// 	let tweetData = null;
+// 	try {
+// 		tweetData = await getTweets(id);
+// 	} catch (e) {
+// 		console.log('ERROR', e);
+// 	}
+// 	return {
+// 		id,
+// 		metadata: JSON.stringify(tweetData),
+// 	};
+// }
 
 export function replaceExamples<T extends Node>(prisma: PrismaClient) {
 	return (tree: T) =>
