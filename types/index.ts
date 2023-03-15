d5eedeed3f4beecdcca2679ef2ef5eca110824a8
Domen <domen.lisjak@gmail.com>
2023-03-15 09:38:48 +0100

    add new types

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,6 @@ export * from "./_site";
 export * from "./common";
 export * from "./http";
 export * from "./icon";
-export * from "./mdx";
 export * from "./seo";
 export * from "./settings";
 export * from "./twitter";
--- types/mdx.ts
+++
@@ -1,6 +0,0 @@
-import type { Post } from "@prisma/client";
-
-export interface MdxCardData extends Pick<Post, "description" | "image"> {
-  name: string | null;
-  url: string | null;
-}
