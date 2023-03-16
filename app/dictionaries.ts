81280016dd0f90cc744e5a9ef483f62597c6a590
Domen <domen.lisjak@gmail.com>
2023-03-16 20:10:19 +0100

    add nl translateion

--- app/dictionaries.ts
+++ app/dictionaries.ts
@@ -3,6 +3,7 @@
 const dictionaries: any = {
   en: () => import("../dictonaries/en.json").then((module) => module.default),
   de: () => import("../dictonaries/de.json").then((module) => module.default),
+  nl: () => import("../dictonaries/nl.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: any) => dictionaries[locale]();
