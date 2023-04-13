a0343590c8d3ffd8ad6a5e7ebf46e788209927ca
Domen <domen.lisjak@gmail.com>
2023-04-13 21:46:59 +0200

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import { cal, inter } from "@/styles/fonts";
+import Script from "next/script";
 
 import "@/styles/globals.css";
 
@@ -12,6 +13,19 @@ export default function App({
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0VCBCMGGN2-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-0VCBCMGGN2-1');
+            `}
+      </Script>
       <main className={`${cal.variable} ${inter.variable})`}>
         <Component {...pageProps} />
         <Toaster position="bottom-center" />
