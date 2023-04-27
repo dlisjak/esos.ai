d066fa5f2fe64be2ef83bac28bab28363205c4e9
Domen <domen.lisjak@gmail.com>
2023-04-27 20:02:19 +0200

    add sitemap

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -2,12 +2,13 @@ import prisma from "@/lib/prisma";
 
 export default async function handler(req: any, res: any) {
   const { subdomain } = req.query;
+  console.log(subdomain);
 
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/xml");
 
   // Instructing the Vercel edge to cache the file
-  res.setHeader("Cache-control", "stale-while-revalidate, s-maxage=3600");
+  // res.setHeader("Cache-control", "stale-while-revalidate, s-maxage=3600");
 
   const site = await prisma.site.findFirst({
     where: {
